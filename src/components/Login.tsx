a0406ea72af90d7393f6948bbdb6557cc1d08080
James Hammond <james@thegruppetto.com>
2024-03-09 14:42:42 -0700

    Add useLocation hook to ProtectedRoute component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,146 +1,143 @@
-import { useEffect, useState } from 'react';
-import { supabase } from '../utils/supabaseClient';
-import { Box, Button, Checkbox, Container, FormControlLabel, Grid, Link, TextField, Typography } from '@mui/material';
-import useToast from '../hooks/useToast';
-import ForgotPasswordDialog from './dialogs/ForgotPasswordDialog';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabaseClient";
+import { Box, Button, Checkbox, Container, FormControlLabel, Grid, Link, TextField, Typography } from "@mui/material";
+import useToast from "../hooks/useToast";
+import ForgotPasswordDialog from "./dialogs/ForgotPasswordDialog";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Login() {
-    const { session } = useAuth();
-    const nav = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [rememberMe, setRememberMe] = useState(false);
-    const [loginButtonEnabled, setLoginButtonEnabled] = useState(true);
+  const { session } = useAuth();
+  const location = useLocation();
+  const nav = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+  const [loginButtonEnabled, setLoginButtonEnabled] = useState(true);
 
-    const { showError } = useToast();
-    const [openPasswordResetDialog, setOpenPasswordResetDialog] = useState(false);
+  const { showError } = useToast();
+  const [openPasswordResetDialog, setOpenPasswordResetDialog] = useState(false);
 
-    function handleClosePasswordResetDialog() {
-      setOpenPasswordResetDialog(false);
+  function handleClosePasswordResetDialog() {
+    setOpenPasswordResetDialog(false);
+  }
+
+  useEffect(() => {
+    const storedEmail = localStorage.getItem("email");
+    const storedPassword = localStorage.getItem("password");
+
+    if (storedEmail && storedPassword) {
+      setRememberMe(true);
+      setEmail(storedEmail);
+      setPassword(storedPassword);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (session) {
+      nav(location.state?.from?.pathname || "/", { replace: true });
+    }
+  }, [session, nav, location.state?.from?.pathname]);
+
+  async function signInWithEmail(event: { preventDefault: () => void }) {
+    event.preventDefault();
+    setLoginButtonEnabled(false);
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password
+    });
+
+    if (error) {
+      showError("Error: Invalid username or password");
+      setLoginButtonEnabled(true);
+    } else {
+      if (rememberMe) {
+        localStorage.setItem("email", email);
+        localStorage.setItem("password", password);
+      } else {
+        localStorage.removeItem("email");
+        localStorage.removeItem("password");
+      }
     }
 
-    useEffect(() => {
-      const storedEmail = localStorage.getItem('email');
-      const storedPassword = localStorage.getItem('password');
+    setLoginButtonEnabled(true);
+    const from = location.state?.from?.pathname || "/";
+    nav(from, { replace: true });
+  }
 
-      if (storedEmail && storedPassword) {
-        setRememberMe(true);
-        setEmail(storedEmail);
-        setPassword(storedPassword);
-      }
-    }, []);
-
-    useEffect(() => {
-      if(session) {
-        nav('/');
-      }
-    }, [session, nav]);
-    
-    async function signInWithEmail(event: { preventDefault: () => void; }) {
-      event.preventDefault();
-        setLoginButtonEnabled(false);
-
-        const { error } = await supabase.auth.signInWithPassword({
-            email: email,
-            password: password,
-        });
-
-        if(error) {
-          showError("Error: Invalid username or password");
-        }
-        
-        if (rememberMe) {
-          localStorage.setItem('email', email);
-          localStorage.setItem('password', password);
-        } else {
-          localStorage.removeItem('email');
-          localStorage.removeItem('password');
-        }
-
-        setLoginButtonEnabled(true);
-        nav('/');
-    }
-
-    return (
-      <Container component="main" maxWidth="sm">
-        <ForgotPasswordDialog open={openPasswordResetDialog} handleClose={handleClosePasswordResetDialog}/>
-        <Box
-          sx={{
-            boxShadow: 3,
-            borderRadius: 2,
-            px: 4,
-            py: 6,
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={signInWithEmail} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              inputProps={{
-                autoCapitalize: 'none',
-              }}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-            <FormControlLabel
-              control={
-                <Checkbox 
-                  value={rememberMe} 
-                  color="primary" 
-                  onChange={(e) => setRememberMe(e.target.checked)}/>}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              disabled={!loginButtonEnabled}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link variant="body2" href="#" onClick={() => { 
+  return (
+    <Container component="main" maxWidth="sm">
+      <ForgotPasswordDialog open={openPasswordResetDialog} handleClose={handleClosePasswordResetDialog} />
+      <Box
+        sx={{
+          boxShadow: 3,
+          borderRadius: 2,
+          px: 4,
+          py: 6,
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={signInWithEmail} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            inputProps={{
+              autoCapitalize: "none"
+            }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <FormControlLabel
+            control={<Checkbox value={rememberMe} color="primary" onChange={(e) => setRememberMe(e.target.checked)} />}
+            label="Remember me"
+          />
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} disabled={!loginButtonEnabled}>
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link
+                variant="body2"
+                href="#"
+                onClick={() => {
                   setOpenPasswordResetDialog(true);
-                }}>
-                  Forgot password?
-                </Link>
-              </Grid>
+                }}
+              >
+                Forgot password?
+              </Link>
             </Grid>
-          </Box>
+          </Grid>
         </Box>
-      </Container>
-    );
+      </Box>
+    </Container>
+  );
 }
