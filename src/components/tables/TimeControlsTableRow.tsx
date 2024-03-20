850a17524635a6e7f0b8a3ccd5c9991e409a6a5c
hammonjj <james@thegruppetto.com>
2024-03-20 10:14:56 -0600

    Refactor TimeControlsTableRow component

--- src/components/tables/TimeControlsTableRow.tsx
+++ src/components/tables/TimeControlsTableRow.tsx
@@ -1,8 +1,19 @@
 import { useState } from "react";
 import { AggregatedResult } from "./TimeControlsTable";
-import { Box, Collapse, IconButton, Table, TableBody, TableCell, TableHead, TableRow, Typography, useMediaQuery } from "@mui/material";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import {
+  Box,
+  Collapse,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+  useMediaQuery
+} from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import StatsBar from "../StatsBar";
 
 interface TimeControlsTableRow {
@@ -11,36 +22,51 @@ interface TimeControlsTableRow {
 
 const TimeControlsTableRow: React.FC<TimeControlsTableRow> = ({ row }) => {
   const [open, setOpen] = useState(false);
-  const isMobile = useMediaQuery('(max-width:600px)');
+  const isMobile = useMediaQuery("(max-width:600px)");
   const padding = "16px";
-  
+
+  const colSpan = isMobile ? 4 : 7;
   return (
     <>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
         <TableCell>
           <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </TableCell>
-        <TableCell component="th" scope="row">{row.variant}</TableCell>
-        <TableCell align="right" style={{ padding: padding }}>{row.totalGames}</TableCell>
+        <TableCell component="th" scope="row">
+          {row.variant}
+        </TableCell>
+        <TableCell align="right" style={{ padding: padding }}>
+          {row.totalGames}
+        </TableCell>
         {!isMobile && (
           <>
-            <TableCell align="right" style={{ padding: padding }}>{row.wins}</TableCell>
-            <TableCell align="right" style={{ padding: padding }}>{row.draws}</TableCell>
-            <TableCell align="right" style={{ padding: padding }}>{row.losses}</TableCell>
-          </>)}
+            <TableCell align="right" style={{ padding: padding }}>
+              {row.wins}
+            </TableCell>
+            <TableCell align="right" style={{ padding: padding }}>
+              {row.draws}
+            </TableCell>
+            <TableCell align="right" style={{ padding: padding }}>
+              {row.losses}
+            </TableCell>
+          </>
+        )}
         <TableCell>
-          <StatsBar stats={{ 
-            wins: row.wins, 
-            losses: row.losses, 
-            draws: row.draws, 
-            total: row.totalGames }} 
-            exclude={0}/>
+          <StatsBar
+            stats={{
+              wins: row.wins,
+              losses: row.losses,
+              draws: row.draws,
+              total: row.totalGames
+            }}
+            exclude={0}
+          />
         </TableCell>
       </TableRow>
       <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={colSpan}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
               <Typography variant="h6" gutterBottom component="div">
@@ -60,21 +86,24 @@ const TimeControlsTableRow: React.FC<TimeControlsTableRow> = ({ row }) => {
                 <TableBody>
                   {row.platforms.map((platformDetail) => (
                     <TableRow key={platformDetail.platform}>
-                      <TableCell component="th" scope="row">{platformDetail.platform}</TableCell>
+                      <TableCell component="th" scope="row">
+                        {platformDetail.platform}
+                      </TableCell>
                       <TableCell align="right">{platformDetail.games}</TableCell>
                       <TableCell align="right">{platformDetail.wins}</TableCell>
                       <TableCell align="right">{platformDetail.draws}</TableCell>
                       <TableCell align="right">{platformDetail.losses}</TableCell>
-                      <TableCell align="right">{`${Math.floor((platformDetail.wins/platformDetail.games)*100)}%/${Math.floor((platformDetail.draws/platformDetail.games)*100)}%/${Math.floor((platformDetail.losses/platformDetail.games)*100)}%`}</TableCell>
-                      {/* <TableCell>
-                        <StatsBar stats={{ 
-                          wins: platformDetail.wins, 
-                          losses: platformDetail.losses, 
-                          draws: platformDetail.draws, 
-                          total: platformDetail.games 
-                          }} 
-                          exclude={0}/>
-                      </TableCell> */}
+                      <TableCell sx={{ width: "45%" }}>
+                        <StatsBar
+                          stats={{
+                            wins: platformDetail.wins,
+                            losses: platformDetail.losses,
+                            draws: platformDetail.draws,
+                            total: platformDetail.games
+                          }}
+                          exclude={0}
+                        />
+                      </TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
